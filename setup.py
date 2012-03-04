a8b747f822241947374ff75fcfd660ee4f341e8f
Andrei Fokau <andrei.fokau@neutron.kth.se>
2012-03-04 12:52:07 +0100

    fix download url

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     author           = 'Andrei Fokau',
     author_email     = 'andrei@5monkeys.se',
     url              = 'https://github.com/andreif/opendata',
-    download_url     = 'https://github.com/andreif/opendata/tarball/master',
+    download_url     = 'https://nodeload.github.com/andreif/opendata/tarball/master',
     version          = __import__(name).__version__,
     packages         = [name],
     include_package_data = True,
